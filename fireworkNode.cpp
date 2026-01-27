1b67b186e724c205dcb15e0706850729029454c4
Ivan <elderivanargueta04@gmail.com>
2026-01-27 12:07:18 -0800

    Revert "Testing and debugging"
    This reverts commit 1af7127b29531dbc4c97b7391c4fac398dd40b6d.

--- fireworkNode.cpp
+++ fireworkNode.cpp
@@ -2,144 +2,200 @@
 // Created by elder on 1/26/2026.
 //
 
+#include <iostream>
 #include <memory>
 #include <vector>
 
 #include "particle.h"
+#include <random>
+#include <thread>
+#include <chrono>
 
-#include "fireworkNode.h"
+class Firework {
+public:
+    enum SizeType {
+        UNUSED = 0,
+        SMALL,
+        MEDIUM,
+        LARGE,
+        EXTRALARGE,
+    };
 
+    struct Payload {
+        unsigned int type;
+        unsigned int count;
 
-void Firework::initRules() {
-    rules.clear();
-    rules.resize(5);
-
-    rules[0].type = UNUSED;
-    rules[0].init(0);
-
-    rules[1].type = SMALL;
-    rules[1].maxAge = 1.5f;
-    rules[1].minAge = 0.5f;
-    rules[1].init(0);
-
-    rules[2].type = MEDIUM;
-    rules[2].maxAge = 1.5f;
-    rules[2].minAge = 0.5f;
-    rules[2].init(1);
-    rules[2].payloads[0].set(SMALL, 8);
-
-    rules[3].type = LARGE;
-    rules[3].maxAge = 1.5f;
-    rules[3].minAge = 0.5f;
-    rules[3].init(1);
-    rules[3].payloads[0].set(MEDIUM, 4);
-
-    rules[4].type = EXTRALARGE;
-    rules[4].maxAge = 1.5f;
-    rules[4].minAge = 0.5f;
-    rules[4].init(2);
-    rules[4].payloads[0].set(LARGE, 2);
-    rules[4].payloads[1].set(MEDIUM, 4);
-}
-
-void Firework::addNode(std::shared_ptr<FireworkNode> &node) {
-    if (root == nullptr) {
-        root = node;
-        return;
-    }
-
-    int depth = 0;
-    std::shared_ptr<FireworkNode> current = root;
-
-    while (true) {
-        if (depth % 2 == 0) {
-            if (node->particle.getPosition().x >= current->particle.getPosition().x) {
-                if (current->right == nullptr) {
-                    current->right = node;
-                    return;
-                }
-                current = current->right;
-            } else if (node->particle.getPosition().x <= current->particle.getPosition().x) {
-                if (current->left == nullptr) {
-                    current->left = node;
-                    return;
-                }
-                current = current->left;
-            }
-        } else if (depth % 2 == 1) {
-            if (node->particle.getPosition().y >= current->particle.getPosition().y) {
-                if (current->right == nullptr) {
-                    current->right = node;
-                    return;
-                }
-                current = current->right;
-            } else if (node->particle.getPosition().y <= current->particle.getPosition().y) {
-                if (current->left == nullptr) {
-                    current->left = node;
-                    return;
-                }
-                current = current->left;
-            }
+        void set(unsigned int passedType, unsigned int passedCount) {
+            type = passedType;
+            count = passedCount;
         }
-        depth++;
+
+        Payload () : type(UNUSED), count(0) {}
+        Payload (unsigned int type, unsigned int count) : type(type), count(count) {}
+    };
+
+    struct Rule {
+        float minAge;
+        float maxAge;
+
+        unsigned int type;
+        unsigned int payloadCount;
+
+        std:: vector<Payload> payloads;
+
+        void init (unsigned int passedPayloadCount) {
+            payloadCount = passedPayloadCount;
+            payloads.resize(payloadCount);
+        }
+
+        Rule() : minAge(0), maxAge(0), type(UNUSED), payloadCount(0) {}
+        Rule(float minAge, float maxAge, unsigned int type, unsigned int payloadCount) : minAge(minAge), maxAge(maxAge), type(type), payloadCount(payloadCount) {}
+    };
+
+    struct FireworkNode {
+        std:: shared_ptr<FireworkNode> left;
+        std:: shared_ptr<FireworkNode> right;
+        Particle particle{};
+        float age;
+        unsigned int type;
+        std:: string name;
+
+        FireworkNode(const std:: string& name) : left(nullptr), right(nullptr), age(0), type(UNUSED), name(name) {
+            std:: cout << name << " node has been created" << std:: endl;
+        }
+        FireworkNode(const std::shared_ptr<FireworkNode> &left, const std::shared_ptr<FireworkNode> &right, const float age, const unsigned int type) : left(left), right(right), age(age), type(type) {}
+
+        ~FireworkNode() {
+            std:: cout << name << " node has been destroyed" << std:: endl;
+        }
+    };
+private:
+    std::shared_ptr<FireworkNode> root;
+
+public:
+    Firework(): root(nullptr){}
+    std:: vector<std::shared_ptr<FireworkNode>> nodes;
+    std:: vector<Rule> rules;
+
+    void initRules() {
+        rules.clear();
+        rules.resize(5);
+
+        rules[0].type = UNUSED;
+        rules[0].init(0);
+
+        rules[1].type = SMALL;
+        rules[1].maxAge = 1.5f;
+        rules[1].minAge = 0.5f;
+        rules[1].init(0);
+
+        rules[2].type = MEDIUM;
+        rules[2].maxAge = 1.5f;
+        rules[2].minAge = 0.5f;
+        rules[2].init(1);
+        rules[2].payloads[0].set(SMALL, 8);
+
+        rules[3].type = LARGE;
+        rules[3].maxAge = 1.5f;
+        rules[3].minAge = 0.5f;
+        rules[3].init(1);
+        rules[3].payloads[0].set(MEDIUM, 4);
+
+        rules[4].type = EXTRALARGE;
+        rules[4].maxAge = 1.5f;
+        rules[4].minAge = 0.5f;
+        rules[4].init(2);
+        rules[4].payloads[0].set(LARGE, 2);
+        rules[4].payloads[1].set(MEDIUM, 4);
     }
-}
 
-float Firework::distance2(std::shared_ptr<FireworkNode> &node1, std::shared_ptr<FireworkNode> &node2) {
-    float xSum = node1->particle.getPosition().x - node2->particle.getPosition().x;
-    float ySum = node1->particle.getPosition().y - node2->particle.getPosition().y;
-
-    float xSquared = xSum * xSum;
-    float ySquared = ySum * ySum;
-
-    return xSquared + ySquared;
-}
-
-void Firework::findNearestNeighborHelper(std::shared_ptr<FireworkNode> *current, std::shared_ptr<FireworkNode> *target,
-                                         std::shared_ptr<FireworkNode> *bestNode, float &bestDistance, int depth) {
-    if (*current == nullptr) {
-        return;
-    }
-
-    int axis = depth % 2;
-    float currentDistance = distance2(*current, *target);
-
-    if (currentDistance > 0.0f && currentDistance < bestDistance) {
-        bestDistance = currentDistance;
-        *bestNode = *current;
-    }
-
-    float targetValueAxis = (axis % 2 == 0) ? target->get()->particle.getPosition().x : target->get()->particle.getPosition().y;
-    float currentValueAxis = (axis % 2 == 0) ? current->get()->particle.getPosition().x : current->get()->particle.getPosition().y;
-
-    std::shared_ptr<FireworkNode> nearChild = (targetValueAxis < currentValueAxis) ? current->get()->left : current->get()->right;
-    std::shared_ptr<FireworkNode> farChild = (targetValueAxis < currentValueAxis) ? current->get()->right : current->get()->left;
-
-    findNearestNeighborHelper(&nearChild, target, bestNode, bestDistance, depth + 1);
-
-    float difference = targetValueAxis - currentValueAxis;
-    float differenceSquared = difference * difference;
-
-    if (differenceSquared < bestDistance) {
-        findNearestNeighborHelper(&farChild, target, bestNode, bestDistance, depth + 1);
-    }
-}
-
-std::shared_ptr<Firework::FireworkNode> Firework::findNearestNeighbor(std::shared_ptr<FireworkNode> &target) {
+    void addNode(std:: shared_ptr<FireworkNode> &node) {
         if (root == nullptr) {
-            std:: cout << "Tree is null returning target" << std:: endl;
-            return target;
+            root = node;
+            return;
         }
 
-        float bestDistance = std::numeric_limits<float>::infinity();
-        std::shared_ptr<FireworkNode> bestNode = root;
+        int depth = 0;
+        std:: shared_ptr<FireworkNode> current = root;
 
-        findNearestNeighborHelper(&root, &target, &bestNode, bestDistance, 0);
+        while (true) {
+            if (depth % 2 == 0) {
+                if (node->particle.getPosition().x >= current->particle.getPosition().x) {
+                    if (current->right == nullptr) {
+                        current->right = node;
+                        return;
+                    }
+                    current = current->right;
+                } else if (node->particle.getPosition().x <= current->particle.getPosition().x) {
+                    if (current->left == nullptr) {
+                        current->left = node;
+                        return;
+                    }
+                }
+            } else if (depth % 2 == 1) {
+                if (node->particle.getPosition().y >= current->particle.getPosition().y) {
+                    if (current->right == nullptr) {
+                        current->right = node;
+                        return;
+                    }
+                    current = current->right;
+                } else if (node->particle.getPosition().y <= current->particle.getPosition().y) {
+                    if (current->left == nullptr) {
+                        current->left = node;
+                        return;
+                    }
+                }
+            }
+            depth++;
+        }
+    }
 
-        return bestNode;
-}
+    FireworkNode *getRoot() {
+        return root.get();
+    }
+};
 
+int main() {
+    std:: random_device gen;
+    Firework firework;
 
-Firework::FireworkNode *Firework::getRoot() {
-    return root.get();
+    firework.nodes.resize(100);
+    firework.initRules();
+
+    auto node1 = std::make_shared<Firework::FireworkNode>("Node1");
+    node1->particle.setPosition(30,34,0);
+    auto node2 = std::make_shared<Firework::FireworkNode>("Node2");
+    node2->particle.setPosition(12,46,0);
+    auto node3 = std::make_shared<Firework::FireworkNode>("Node3");
+    node3->particle.setPosition(52,22,0);
+    std:: cout << std:: endl;
+
+    std:: cout << "Address of node1: " << node1.get() << std::endl;
+    std:: cout << "Address of node2: " << node2.get() << std::endl;
+    std:: cout << "Address of node3: " << node3.get() << std::endl;
+
+    if (firework.getRoot() == nullptr) {
+        std:: cout << "Root is nullptr" << std:: endl;
+    } else {
+        std:: cout << "Root is not nullptr" << std:: endl;
+    }
+
+    firework.nodes.push_back(node1);
+    firework.nodes.push_back(node2);
+    firework.nodes.push_back(node3);
+    std:: cout << std:: endl;
+
+    
+
+    for (auto &node : firework.nodes) {
+        if (node == nullptr) continue;
+        std:: cout << node->name << " address: " << node.get() << std:: endl;
+        firework.addNode(node);
+    }
+    std:: cout << std:: endl;
+
+    std:: cout << "Address of root node: " << firework.getRoot() << std:: endl;
+    std:: cout << "Address of root-left: " << firework.getRoot()->left.get() << std:: endl;
+    std:: cout << "Address of root-right: " << firework.getRoot()->right.get() << std:: endl;
+    std:: cout << std:: endl;
 }
